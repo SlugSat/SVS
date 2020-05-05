c7359f91cc3858f97d5c118f3662981b0b0c471d
Juan Ayala <ayala.juan40@gmail.com>
2020-05-05 02:06:40 -0700

    completed LED_irradiance

--- Matlab/LED_irradiance.m
+++ Matlab/LED_irradiance.m
@@ -1,7 +1,7 @@
-function [surface] = irradiance( position,intensity, resolution)
+function [x_surf y_surf surface] = LED_irradiance( position,intensity, resolution)
 
-    [x_surf y_surf] = meshgrid(0:resolution:50);
-    
+    [x_surf y_surf] = meshgrid(0:resolution:0.05);
+    position = position + [0.025 0.025 0];
     surface = (position(3).^2.*intensity)./(((x_surf-position(1)).^2 + (y_surf-position(2)).^2 + position(3).^2)).^2;
 
 end
